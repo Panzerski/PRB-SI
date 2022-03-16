5b480dfe2633f56d8b714d92c426676a4646a9f6
Michał Rogowski <85677540+Panzerski@users.noreply.github.com>
2022-03-16 11:17:52 +0100

    Add files via upload

--- kod1.py
+++ kod1.py
@@ -1,6 +1,18 @@
 import csv
+from random import randint
 
-with open("items_PRB.csv","r") as file:
-    reader=csv.reader(file)
-    for row in reader:
-        print(row)
+limit = 1000
+#rand = randint(1, 1001)
+suma = 0
+wyniki = []
+with open("items_PRB.csv", "r") as file:
+    reader = csv.reader(file)
+    while suma <= limit:
+        rand = randint(1, 1001)
+        for row in reader:
+            if int(row[0]) == rand and int(row[2]) <= 1000:
+                print(row)
+                suma += int(row[2])
+                wyniki.append(row)
+
+print(wyniki)
