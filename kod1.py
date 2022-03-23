e786bec3786da5b7dc066d8b07b7cede0db842e9
Konrad Błaszkiewicz <65293869+mbkonrad@users.noreply.github.com>
2022-03-23 11:21:17 +0100

    Update kod1.py

--- kod1.py
+++ kod1.py
@@ -7,12 +7,16 @@ suma = 0
 wyniki = []
 with open("items_PRB.csv", "r") as file:
     reader = csv.reader(file)
-    while suma <= limit:
-        rand = randint(1, 1001)
+    while True:
+        next(reader)
+        rand = randint(1, 999)
         for row in reader:
-            if int(row[0]) == rand and int(row[2]) <= 1000:
-                print(row)
+            if int(row[0]) == rand:
+                if int(row[2]) > limit-suma: break
                 suma += int(row[2])
                 wyniki.append(row)
+                print(suma)
+        if suma >= limit: break
+        file.seek(0)
 
 print(wyniki)
