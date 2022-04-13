33c0c301c982841ba111b9ae5e917c4955ecd521
Michał Rogowski <85677540+Panzerski@users.noreply.github.com>
2022-04-13 12:13:51 +0200

    HM/HMS

--- kod1.py
+++ kod1.py
@@ -2,23 +2,33 @@ import csv
 from random import randint
 
 limit = 1000
-suma = 0
-waga = 0
+HM = []
+HMS = 10
+suma = [0] * HMS
+waga = [0] * HMS
 wyniki = []
+
 with open("items_PRB.csv", "r") as file:
     reader = csv.reader(file)
-    while True:
-        next(reader)
-        rand = randint(1, 1000)
-        for row in reader:
-            if int(row[0]) == rand:
-                if int(row[2]) > limit - suma: break
-                suma += int(row[2])
-                waga += float(row[3])
-                wyniki.append(row)
-                #print(suma)
-        if suma >= limit: break
+    for i in range(0, HMS):
+        wyniki.clear()
+        while True:
+            next(reader)
+            rand = randint(1, 1000)
+            for row in reader:
+                if int(row[0]) == rand:
+                    if int(row[2]) > limit - suma[i]:
+                        break
+                    suma[i] += int(row[2])
+                    waga[i] += float(row[3])
+                    wyniki.append(row)
+                    # print(suma)
+            if suma[i] >= limit:
+                break
+            file.seek(0)
         file.seek(0)
-print("waga:  %.1f" % waga)
-print("cena: ",suma)
-print(wyniki)
+        HM.append(wyniki)
+        print("waga:  %.1f" % waga[i])
+        print("cena: ", suma[i])
+
+print(HM)
